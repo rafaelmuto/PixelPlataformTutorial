e9eac23426e461129975d52025fb38c1af33e92b
rafaelmuto <r.nagahama@gmail.com>
2022-10-17 20:31:04 -0300

    hitboxes

--- HitBox/HitBox.gd
+++ HitBox/HitBox.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
-
-func _on_Spikes_body_entered(body):
-	print('spike._on_Spikes_body_entered: ' + body.name)
+	
+func _on_HitBox_body_entered(body):
+	print('HitBox_body_entered: ' + body.name)
 	if body is PlayerCharacter:
 		get_tree().reload_current_scene()
 #		body.queue_free()
