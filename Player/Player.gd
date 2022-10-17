97ad22ae7ae1e1977ac02a5956d407fcdde1e27b
rafaelmuto <r.nagahama@gmail.com>
2022-10-17 20:58:50 -0300

    better organization and movement profiles

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,14 +1,7 @@
 extends KinematicBody2D
-
 class_name PlayerCharacter
 
-export (int) var GRAVITY = 5
-export (int) var ADDITIONAL_FALL_GRAVITY = 2
-export (int) var JUMP_FORCE = -160
-export (int) var JUMP_RELEASE_FORCE = -40
-export (int) var MAX_SPEED = 75
-export (int) var ACCELERATION = 10
-export (int) var FRICTION = 10
+export (Resource) var moveData
 
 var velocity = Vector2.ZERO
 
@@ -17,7 +10,7 @@ onready var animatedSprite = $AnimatedSprite
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print('Hello World!! from Player Scene...')
-	animatedSprite.frames = load("res://Player/PlayerGreenSkin.tres")
+	animatedSprite.frames = load("res://Player/Skin/PlayerGreenSkin.tres")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -39,14 +32,14 @@ func _physics_process(_delta):
 
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
-			velocity.y = JUMP_FORCE
+			velocity.y = moveData.JUMP_FORCE
 	else:
 		animatedSprite.animation = 'jump'
-		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
-			velocity.y = JUMP_RELEASE_FORCE
+		if Input.is_action_just_released("ui_up") and velocity.y < moveData.JUMP_RELEASE_FORCE:
+			velocity.y = moveData.JUMP_RELEASE_FORCE
 			
 		if velocity.y > 0:
-			velocity.y += ADDITIONAL_FALL_GRAVITY
+			velocity.y += moveData.ADDITIONAL_FALL_GRAVITY
 	
 	var was_in_air = not is_on_floor()
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -57,11 +50,11 @@ func _physics_process(_delta):
 		animatedSprite.frame = 1
 	
 func apply_gravity():
-	velocity.y += GRAVITY
+	velocity.y += moveData.GRAVITY
 	velocity.y = min(velocity.y, 300)
 	
 func apply_friction():
-	velocity.x = move_toward(velocity.x, 0, FRICTION)
+	velocity.x = move_toward(velocity.x, 0, moveData.FRICTION)
 	
 func apply_acceleration(amount: float):
-	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
+	velocity.x = move_toward(velocity.x, moveData.MAX_SPEED * amount, moveData.ACCELERATION)
