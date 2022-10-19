aeccffdbb125fd5d38f6307900259e31da03b5d2
rafaelmuto <r.nagahama@gmail.com>
2022-10-18 22:39:23 -0300

    doble jump, coyote jump and jump buffer

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,17 +9,19 @@ var state = MOVE
 
 onready var animatedSprite = $AnimatedSprite
 onready var ladderCheck = $LadderCheck
+onready var jumpBufferTimer = $JumpBufferTimer
+onready var coyoteJumpTimer = $CoyoteJumpTimer
 
+
+var double_jump
+var buffered_jump = false
+var coyote_jump = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animatedSprite.frames = load("res://Player/Skin/PlayerGreenSkin.tres")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+	moveData = moveData as MovementData
+	double_jump = moveData.DOUBLE_JUMP_COUNT
 
 func _physics_process(_delta):
 	var input = Vector2.ZERO
@@ -43,26 +45,51 @@ func move_state(input):
 		apply_acceleration(input.x)
 		animatedSprite.animation = 'run'
 		animatedSprite.flip_h = input.x > 0
-
+		
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
-			velocity.y = moveData.JUMP_FORCE
+		double_jump = moveData.DOUBLE_JUMP_COUNT
 	else:
 		animatedSprite.animation = 'jump'
+
+	if can_jump():
+		input_jump()
+	else:
+		# jump release
 		if Input.is_action_just_released("ui_up") and velocity.y < moveData.JUMP_RELEASE_FORCE:
 			velocity.y = moveData.JUMP_RELEASE_FORCE
 			
+		# double jump
+		if Input.is_action_just_pressed("ui_up") and double_jump > 0:
+			velocity.y = moveData.JUMP_FORCE
+			double_jump -= 1
+			# this animation is waaay too fast...
+			# animatedSprite.animation = 'idle'
+			# animatedSprite.frame = 0
+		
+		# buffer jump
+		if Input.is_action_just_pressed("ui_up"):
+			buffered_jump = true
+			jumpBufferTimer.start()
+		
+		# fast fall
 		if velocity.y > 0:
 			velocity.y += moveData.ADDITIONAL_FALL_GRAVITY
 	
 	var was_in_air = not is_on_floor()
+	var was_on_floor = is_on_floor()
 	
+	# move_and_slide() actualy moves the Player
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	var just_landed = is_on_floor() and was_in_air
 	if just_landed:
 		animatedSprite.animation = 'run'
 		animatedSprite.frame = 1
+		
+	var just_left_ground = not is_on_floor() and was_on_floor
+	if just_left_ground and velocity.y >= 0:
+		coyote_jump = true
+		coyoteJumpTimer.start()
 
 
 func climb_state(input):
@@ -77,8 +104,20 @@ func climb_state(input):
 	velocity = move_and_slide(velocity, Vector2.UP)
 
 
+func can_jump():
+	return is_on_floor() or coyote_jump
+
+
+func input_jump():
+	if Input.is_action_just_pressed("ui_up") or buffered_jump:
+			velocity.y = moveData.JUMP_FORCE
+			buffered_jump = false
+			coyote_jump = false
+
+
 func is_on_ladder():
 	return ladderCheck.is_colliding()
+
 
 func apply_gravity():
 	velocity.y += moveData.GRAVITY
@@ -92,3 +131,10 @@ func apply_friction():
 func apply_acceleration(amount: float):
 	velocity.x = move_toward(velocity.x, moveData.MAX_SPEED * amount, moveData.ACCELERATION)
 
+
+func _on_JumpBufferTimer_timeout():
+	buffered_jump = false
+
+
+func _on_CoyoteJumpTimer_timeout():
+	pass # Replace with function body.
