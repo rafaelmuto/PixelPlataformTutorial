71ecee5e46ad9385df4a01290f8d96e109414ef4
rafaelmuto <r.nagahama@gmail.com>
2022-10-19 22:45:22 -0300

    flow path enemy

---
+++ MovingSpikeEnemy.gd
@@ -0,0 +1,21 @@
+tool
+extends Path2D
+
+enum ANIMATION_TYPE { LOOP, BOUNCE }
+
+export (ANIMATION_TYPE) var animation_type setget set_animation_type
+
+#onready var animationPlayer = $AnimationPlayer
+
+func _ready():
+	play_updated_animation()
+	
+
+func play_updated_animation():
+	match animation_type:
+		ANIMATION_TYPE.LOOP: $AnimationPlayer.play("MoveAlongPathLoop")
+		ANIMATION_TYPE.BOUNCE: $AnimationPlayer.play("MoveAlongPathBounce")
+
+func set_animation_type(value):
+	animation_type = value
+	play_updated_animation()
