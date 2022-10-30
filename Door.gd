54a439578ee54a0664868e2d65c651deb9e82977
rafaelmuto <r.nagahama@gmail.com>
2022-10-30 01:33:00 -0300

    final lesson

--- Door.gd
+++ Door.gd
@@ -2,11 +2,13 @@ extends Area2D
 
 export (String, FILE, "*.tscn") var target_level_path = ""
 
-var over_player = false
+var player = false
 
+# warning-ignore:unused_argument
 func _process(delta):
-	if not over_player: return
-	if Input.is_action_just_pressed("ui_up"):
+	if not player: return
+	if not player.is_on_floor(): return
+	if Input.is_action_just_pressed("ui_accept"):
 		if target_level_path.empty(): return
 		got_to_next_level()
 
@@ -17,12 +19,13 @@ func got_to_next_level():
 	Transitions.play_enter_transition()
 	yield(Transitions, "transition_completed")
 	get_tree().paused = false
+# warning-ignore:return_value_discarded
 	get_tree().change_scene(target_level_path)
 	
 func _on_Door_body_entered(body):
 	if not body is PlayerCharacter: return
-	over_player = true
+	player = body
 
 func _on_Door_body_exited(body):
 	if not body is PlayerCharacter: return
-	over_player = false
+	player = null
